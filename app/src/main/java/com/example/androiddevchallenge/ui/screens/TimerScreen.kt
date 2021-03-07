abb3c379ed10cf0b74e1097b522d7345e3b30293
Piyush  Bulchandani <bulchandani.piyush@gmaiol.com>
2021-03-08 01:49:57 +0530

    minor formatting

--- app/src/main/java/com/example/androiddevchallenge/ui/screens/TimerScreen.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/screens/TimerScreen.kt
@@ -19,35 +19,28 @@ import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.androiddevchallenge.R
 import androidx.compose.foundation.clickable
-import androidx.compose.material.Colors
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.PaintingStyle
-import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.layout.ContentScale
 import com.example.androiddevchallenge.ui.screens.TimerState.*
-import com.google.android.material.color.MaterialColors
 import kotlinx.coroutines.*
-import java.text.SimpleDateFormat
-import java.util.*
 import java.util.concurrent.TimeUnit
 
-val DURATION  = 60 *1000L // duration in SEC
-var timer:Job? = null
+val DURATION = 60 * 1000L // duration in SEC
+var timer: Job? = null
+
 @Preview
 @Composable
 fun TimerScreen(
 
 ) {
-    var mills =  remember { mutableStateOf(DURATION)}
-    var state = remember { mutableStateOf(STOPPED)}
+    var mills = remember { mutableStateOf(DURATION) }
+    var state = remember { mutableStateOf(STOPPED) }
 
 
 
-    DrawTimerScreen(mills.value ,state.value,object :TimerListener{
+    DrawTimerScreen(mills.value, state.value, object : TimerListener {
         override fun onStart() {
             state.value = RUNNING
         }
@@ -64,9 +57,9 @@ fun TimerScreen(
         }
 
         override fun onTick(ms: Long) {
-            if (ms>0){
+            if (ms > 0) {
                 mills.value = ms
-            }else{
+            } else {
                 state.value = STOPPED
                 mills.value = DURATION
 
@@ -83,18 +76,23 @@ fun DrawTimerScreen(
     listener: TimerListener
 ) {
 
-    ConstraintLayout(modifier = Modifier
-        .fillMaxSize()
-        .background(color = MaterialTheme.colors.background)) {
-        val (tvTime,timerCircle,btnPlay,btnStop,bgImg) = createRefs()
+    ConstraintLayout(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colors.background)
+    ) {
+        val (tvTime, timerCircle, btnPlay, btnStop, bgImg) = createRefs()
 
         //val sdf = SimpleDateFormat("mm:ss", Locale.getDefault())
 
-        Image(/*colorFilter = ColorFilter.tint(Color.Red),*/painter = painterResource(id = R.drawable.bg_pattern),alpha = 0.1f ,contentScale = ContentScale.Crop, contentDescription = "",
+        Image(/*colorFilter = ColorFilter.tint(Color.Red),*/painter = painterResource(id = R.drawable.bg_pattern),
+            alpha = 0.1f,
+            contentScale = ContentScale.Crop,
+            contentDescription = "",
             modifier = Modifier.fillMaxSize()
         )
-        
-        DrawCircle(modifier = Modifier.constrainAs(timerCircle){
+
+        DrawCircle(modifier = Modifier.constrainAs(timerCircle) {
             start.linkTo(parent.start)
             end.linkTo(parent.end)
 
@@ -105,16 +103,17 @@ fun DrawTimerScreen(
                 bottomMargin = 10.dp,
                 bias = 0.4f
             )
-        },Color.Black,mills)
+        }, Color.Black, mills)
 
         Text(
-            String.format("%02d:%02d",
+            String.format(
+                "%02d:%02d",
                 TimeUnit.MILLISECONDS.toMinutes(mills),
                 TimeUnit.MILLISECONDS.toSeconds(mills) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(mills))
             ),
             fontSize = 32.sp,
-            color =Color.Black,
+            color = Color.Black,
             modifier = Modifier.constrainAs(tvTime) {
                 top.linkTo(timerCircle.top)
                 bottom.linkTo(timerCircle.bottom)
@@ -122,25 +121,24 @@ fun DrawTimerScreen(
                 end.linkTo(parent.end)
             })
 
-        DrawPlayBtn(Modifier.constrainAs(btnPlay){
-            top.linkTo(timerCircle.bottom,margin = 100.dp)
+        DrawPlayBtn(Modifier.constrainAs(btnPlay) {
+            top.linkTo(timerCircle.bottom, margin = 100.dp)
             start.linkTo(parent.start)
             end.linkTo(parent.end)
-        },listener,state,mills)
+        }, listener, state, mills)
 
-        DrawStopBtn(Modifier.constrainAs(btnStop){
+        DrawStopBtn(Modifier.constrainAs(btnStop) {
             top.linkTo(btnPlay.top)
             bottom.linkTo(btnPlay.bottom)
-            start.linkTo(btnPlay.end,margin = 15.dp)
-        },listener,state,mills)
-
+            start.linkTo(btnPlay.end, margin = 15.dp)
+        }, listener, state, mills)
 
 
     }
 }
 
 @Composable
-fun DrawCircle(modifier: Modifier, color: Color,mills: Long){
+fun DrawCircle(modifier: Modifier, color: Color, mills: Long) {
 
 
     Canvas(modifier = modifier
@@ -151,31 +149,43 @@ fun DrawCircle(modifier: Modifier, color: Color,mills: Long){
         drawCircle(
             color = color,
             center = middle,
-            radius = size.minDimension/2,
+            radius = size.minDimension / 2,
             style = Stroke(2.dp.toPx()),
         )
 
         var per = 0f
-        if(mills>0 && DURATION>0){
-            per = (DURATION-mills).toFloat()/ DURATION.toFloat()
+        if (mills > 0 && DURATION > 0) {
+            per = (DURATION - mills).toFloat() / DURATION.toFloat()
         }
-        val sweepAngle = per*360
+        val sweepAngle = per * 360
 
-        drawArc(brush = Brush.sweepGradient(listOf(Color.Green,Color.Red)),startAngle = 270f,sweepAngle = sweepAngle.toFloat(),useCenter = true,style =Stroke())
-        drawArc(brush = Brush.sweepGradient(listOf(Color.Green,Color.Green)),startAngle = 270f,sweepAngle = sweepAngle.toFloat(),useCenter = true)
+        drawArc(
+            brush = Brush.sweepGradient(listOf(Color.Green, Color.Red)),
+            startAngle = 270f,
+            sweepAngle = sweepAngle.toFloat(),
+            useCenter = true,
+            style = Stroke()
+        )
+        drawArc(
+            brush = Brush.sweepGradient(listOf(Color.Green, Color.Green)),
+            startAngle = 270f,
+            sweepAngle = sweepAngle.toFloat(),
+            useCenter = true
+        )
 
     })
 }
+
 @Composable
 fun DrawPlayBtn(
     modifier: Modifier, listener: TimerListener, state: TimerState, mills: Long
 
-){
-    when(state){
-        PAUSED,STOPPED
-         ->{
+) {
+    when (state) {
+        PAUSED, STOPPED
+        -> {
             Image(
-                painter = painterResource(id =R.drawable.ic_play),
+                painter = painterResource(id = R.drawable.ic_play),
                 contentDescription = "",
                 modifier = modifier
                     .height(60.dp)
@@ -187,7 +197,7 @@ fun DrawPlayBtn(
         }
         RUNNING -> {
             Image(
-                painter = painterResource(id =R.drawable.ic_pause),
+                painter = painterResource(id = R.drawable.ic_pause),
                 contentDescription = "",
                 modifier = modifier
                     .height(60.dp)
@@ -196,7 +206,7 @@ fun DrawPlayBtn(
                         listener.onPause()
                     }
             )
-            UpdateTime(mills,listener)
+            UpdateTime(mills, listener)
         }
     }
 
@@ -208,12 +218,12 @@ fun DrawPlayBtn(
 fun DrawStopBtn(
     modifier: Modifier, listener: TimerListener, state: TimerState, mills: Long
 
-){
-    when(state){
+) {
+    when (state) {
         RUNNING
-        ->{
+        -> {
             Image(
-                painter = painterResource(id =R.drawable.ic_stop),
+                painter = painterResource(id = R.drawable.ic_stop),
                 contentDescription = "",
                 modifier = modifier
                     .height(30.dp)
@@ -235,7 +245,7 @@ fun UpdateTime(mills: Long, listener: TimerListener) {
     val coroutineScope = rememberCoroutineScope()
     coroutineScope.launch {
         delay(1000)
-        val newMs = mills-1000
+        val newMs = mills - 1000
         listener.onTick(newMs)
     }
 }
